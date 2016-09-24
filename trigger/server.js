a24e83adca31802b0bc8cc5168fde79a6661d49b
akumar18 <akumar18@ncsu.edu>
2016-09-24 19:39:31 -0400

    Added Express. Splitup server code

--- trigger/server.js
+++ trigger/server.js
@@ -1,43 +1,22 @@
 // Imports
-var http = require('http');
-var exec = require('child_process').exec;
-var fs = require('fs');
+var express = require('express');
 
 // Globals
 const port = 8000;
-const build_cmd = 'ls';
-
-// Log File
-const logfile = 'build_report.log';
+var app = express();
 
 // Server
-var server = http.createServer(onRequest);
-server.listen(port, function(){
+app.listen(port, function(){
     console.log("Server running on: http://127.0.0.0:"+port);
 });
 
-// Request Callback
-function onRequest(req, res) {
-    console.log("Starting Build...");
+// Route Requests
+app.get('/', function(req, res){
+    console.log('History!');
+    res.send('History');
+});
 
-    // Build
-    var build_process = exec(build_cmd, {maxBuffer: 1024 * 5000}, onBuild);
-
-    // Bind Streams
-    build_process.stdout.pipe(res);
-    build_process.stderr.pipe(process.stderr);
-
-    build_process.on('exit', function(){
-        console.log('Build Completed');
-        return true;
-    });
-}
-
-// Build Callback
-function onBuild(err, stdout, stderr) {
-    if (err){
-        console.log(err);
-    }
-
-    fs.writeFileSync(logfile, stdout);
-}
+app.get('/build', function(req, res){
+    console.log('Build!');
+    res.send('Build');
+});
