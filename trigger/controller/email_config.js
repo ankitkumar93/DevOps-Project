7376a670063045543169c29971dbad1963cb4817
akumar18 <akumar18@ncsu.edu>
2016-09-25 21:00:14 -0400

    Added mailure

---
+++ trigger/controller/email_config.js
@@ -0,0 +1,18 @@
+var email_config = {}
+    auth: {
+        service: 'Gmail',
+        user: 'team.mail.devops.build@gmail.com',
+        password: 'qwerty@2016'
+    },
+    options: {
+        from: '"DevOps Support" <team.mail.devops.build@gmail.com>',
+        to: 'vipulkashyap111@gmail.com',
+        subjectfail: 'Build Status - Fail',
+        subjectpass: 'Build Status - Success',
+        textfail: 'Your most recently build failed',
+        textpass: 'Your most recently build was successfull'
+        logurl: '54.213.145.239:8000/log?id='
+    }
+};
+
+module.exports = auth_data;
---
+++ trigger/controller/mailer.js
@@ -0,0 +1,43 @@
+var nodemailer = require('nodemailer');
+var email_config = require('./email_config.js');
+
+var transporter = nodemailer.createTransport({
+    service: email_config.auth.service,
+    auth: {
+        user: email_config.auth.user, // Your email id
+        pass: email_config.auth.password // Your password
+    }
+});
+
+
+// Send Mail
+function sendMail(status, logid){
+    
+    var textToSend;
+    var subjectToSend;
+    if (status == failure){
+        textToSend = email_config.options.textfail;
+        subjectToSend = email_config.options.subjectfail;
+    } else {
+        textToSend = email_config.options.textpass;
+        subjectToSend = email_config.options.subjectpass;
+    }
+
+    var logurl = email_config.options.logurl + logid;
+
+    var mailOptions = {
+        from: email_config.options.from,
+        to: email_config.options.to,
+        subject: email_config.options.subject + textToSend,
+        html: '<a href=' + logurl + '>Log File</a>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
+
+module.exports = sendMail;
