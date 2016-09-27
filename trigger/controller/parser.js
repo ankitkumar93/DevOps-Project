675464a7f9b1955b86214ab9c052b992f0b70008
akumar18 <akumar18@ncsu.edu>
2016-09-27 18:39:17 -0400

    Fixed parser

--- trigger/controller/parser.js
+++ trigger/controller/parser.js
@@ -2,13 +2,12 @@
 var Parser = require('simple-text-parser');
 var parser = new Parser();
 
-var result = "success";
 function parse(data){
+var result = "success";
   parser.addRule(/\d failing/ig, function(tag){
     console.log("Inside add rule",tag);
     if(tag !== '0 failing'){
       result = "failure";
-      console.log(tag);
     }
     return tag;
   });
