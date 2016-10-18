c31d4e9735b4dbb805e858cceaac49c7b5e8c831
akumar18 <akumar18@ncsu.edu>
2016-10-18 15:32:43 -0400

    Added advanced testing

--- build/build.sh
+++ build/build.sh
@@ -7,18 +7,25 @@ echo "SETUP:Finished"
 
 ## Testing
 echo "TEST:Start"
+## Basic Testing with Coverage
 istanbul cover test/1-calc.js
 istanbul cover test/2-postfixer.js
 istanbul cover test/3-app.js
+
+## Advanced Testing with Coverage
+node advanced_testing.js
+istanbul cover test.js
 echo "TEST:Finished"
 
 ## Analysis
 echo "ANALYSIS:Start"
+
+## Static Analysis
 jshint app.js
 jshint calc.js
 jshint postfixer.js
 
-
+## Custom Metrics
 node analysis.js app.js
 node analysis.js calc.js
 node analysis.js postfixer.js
