612ec6a9fc5c8bcf188a2ff3fb73c479d7487553
akumar18 <akumar18@ncsu.edu>
2016-10-18 01:10:28 -0400

    added data split

--- trigger/controller/parser.js
+++ trigger/controller/parser.js
@@ -1,12 +1,16 @@
 "use strict";
 function parse(data){
     var result = "failure";
-    var regex = /\d failing/;
-    var match = data.search(regex);
+    var testData = data.split("TEST:Start")[1].split("TEST:Finished")[0];
+    var analysisData = data.split("ANALYSIS:Start")[1].split("ANALYSIS:Finished")[0];
 
-    if (match == -1)
-        result = "success";
+    console.log("Test\n");
+    console.log(testData);
+    console.log("\n\n\n");
 
+    console.log("Analysis\n");
+    console.log(analysisData);
+    console.log("\n\n\n");
     return result;
 }
 
