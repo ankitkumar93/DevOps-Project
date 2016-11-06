3f83930731ae9916de10a8b8aafa51493cdc849e
akumar18 <akumar18@ncsu.edu>
2016-11-06 01:30:55 -0400

    Added email codE

--- server/monitor.sh
+++ server/monitor.sh
@@ -5,6 +5,7 @@
 CPU_LIMIT=50
 MEM_LIMIT=60
 NET_LIMIT=70
+ADMIN_EMAIL=""
 
 # Monitor
 while [ true ]; do
@@ -13,17 +14,19 @@ while [ true ]; do
 		# CPU Usage	
 		cpu_uage=$(echo $usage | cut -d ' ' -f1)
 		if [ $cpu_usage -g $CPU_LIMIT ]; then
-			
+			mail -s "WebAPP Alert" $ADMIN_EMAIL <<< "ALERT: HIGH CPU USAGE: "$cpu_usage	
 		fi
 		
 		# Memory Usage
 		mem_uage=$(echo $usage | cut -d ' ' -f2)
 		if [ $mem_usage -g $MEM_LIMIT ]; then
+			mail -s "WebAPP Alert" $ADMIN_EMAIL <<< "ALERT: HIGH MEMORY USAGE: "$mem_usage	
 		fi
 		
 		# Network Usage
 		net_uage=$(echo $usage | cut -d ' ' -f3)
 		if [ $net_usage -g $NET_LIMIT ]; then
+			mail -s "WebAPP Alert" $ADMIN_EMAIL <<< "ALERT: HIGH NETWORK USAGE: "$net_usage	
 		fi
 done
 
