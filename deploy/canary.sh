4d9f8510e6656171fde3c9f88f53164a9fe53895
akumar18 <akumar18@ncsu.edu>
2016-11-11 05:34:35 -0500

    D

--- deploy/canary.sh
+++ deploy/canary.sh
@@ -6,7 +6,7 @@ echo "[appserver]" >> inventory
 echo 'node3 ansible_ssh_host='$app_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
 
 ## Deploy
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i canary_inventory configure_canary.yml --limit "appserver"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i canary_inventory /home/ubuntu/DevOps-Project/deploy/ --limit "appserver"
 
 redis_ip=$(cat config.json | jq '.REDIS_IP')
 
