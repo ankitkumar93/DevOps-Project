8a515c769899a5d8eb0aace2f870422e0cb1c8da
akumar18 <akumar18@ncsu.edu>
2016-11-11 05:36:15 -0500

    D

--- deploy/app.sh
+++ deploy/app.sh
@@ -6,4 +6,4 @@ echo "[appserver]" >> inventory
 echo 'node3 ansible_ssh_host='$app_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
 
 ## Deploy
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i canary_inventory /home/ubuntu/DevOps-Project/deploy/ --limit "appserver"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i canary_inventory /home/ubuntu/DevOps-Project/deploy/configure_app.yml --limit "appserver"
